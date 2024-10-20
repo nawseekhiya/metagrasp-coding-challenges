33406310a58503dfb5d4d0036d4fedcab1ed034a
nawseekhiya <araj80317@gmail.com>
2024-10-20 11:08:18 +0530

    Add fibonacciGenerator.java algorithm

---
+++ fibonacciGenerator.java
@@ -0,0 +1,11 @@
+/**
+ * ALGORITHM fibonacciGenerator(n)
+ *  n: positive integer given by user to generate first n sequence of fibonacci
+ *      if n is 0, return 0
+ *      if n is 1, return 1
+ *      else return fibonacci(n-1) + fibonacci(n-2)
+ *  generates the sequences by recursive call of function fibonacci()
+ * the void main() function takes input from user as n,
+ *      if the n is less than 0, it is negative, therefore invalid input
+ *      otherwise, prints the fibonacci sequence by iterative call of fibonacci() from 0 to n-1
+ */
