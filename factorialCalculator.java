be0cfc3d6b8758c4b90ef22993da1fd4e7803f50
nawseekhiya <araj80317@gmail.com>
2024-10-20 12:43:34 +0530

    Add factorialCalculator.java algorithm

---
+++ factorialCalculator.java
@@ -0,0 +1,12 @@
+/**
+ * ALGORIHM factorialCalculator(n)
+ *  n: a positive integer input from the user
+ *      calculates factorial of n using recursion
+ *      displays the result
+ * if n is 0, returns 1
+ * if n is 1, returns 1
+ * otherwise,
+ *  returns n*factorial(n-1)
+ *      the main() checks for number integrity, checks for factorial
+ *      by calling factorial(n), and displays output message accordingly
+ */
