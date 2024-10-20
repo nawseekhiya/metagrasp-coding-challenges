7041a30785e0839631668899aec5a41c7bd5f876
nawseekhiya <araj80317@gmail.com>
2024-10-20 12:47:42 +0530

    Add factorialCalculator.java source code

--- factorialCalculator.java
+++ factorialCalculator.java
@@ -1,3 +1,4 @@
+
 /**
  * ALGORIHM factorialCalculator(n)
  *  n: a positive integer input from the user
@@ -10,3 +11,25 @@
  *      the main() checks for number integrity, checks for factorial
  *      by calling factorial(n), and displays output message accordingly
  */
+
+import java.util.Scanner;
+class factorialCalculator {
+    public int factorial(int n) {
+        if (n == 0 || n == 1)
+            return 1;
+        else
+            return n * factorial(n - 1);
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter a positive number: ");
+        int n = in.nextInt();
+        factorialCalculator obj = new factorialCalculator();
+        if (n < 0)
+            System.out.println(+n + " is not a positive number. Please enter a positive number.");
+        else
+            System.out.println("The factorial of " + n + " : " + obj.factorial(n));
+        in.close();
+    }
+}
