9fc0534867a74beb37509e9ca35b8f1950ab1ea1
nawseekhiya <araj80317@gmail.com>
2024-10-20 13:52:23 +0530

    Add vowelCounter.java algorithm

---
+++ vowelCounter.java
@@ -0,0 +1,15 @@
+/**
+ * ALGORITHM vowelCounter(s)
+ *  s: a String input from user may/may not consist vowels
+ *      checks for presence of vowels in the string
+ *      counts if found vowel
+ * count <- 0
+ * convert the string to lowercase
+ * collecting character by using a loop
+ * if character is 'a' or 'e' or 'i' or 'o' or 'u'
+ *      increment the counter
+ * returns counter
+ * the main() takes string as input from user
+ * gets the value of the counter from vowelCounter()
+ * displays the no. of vowels with appropriate output message
+ */
