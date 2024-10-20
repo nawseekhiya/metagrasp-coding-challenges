244da7b60b6f4d4c3152c7f49ad78750c9bfb1d6
nawseekhiya <araj80317@gmail.com>
2024-10-20 13:26:12 +0530

    Add bubbleSort.c algorithm

---
+++ bubbleSort.c
@@ -0,0 +1,13 @@
+/**
+ * ALGORITHM bubbleSort(a[0..n-1])
+ *  a[] : array of integers taken as input from user
+ *          let n be size of array
+ *          the index of array runs from 0 to n-1
+ *  for i <- 0 to n-1 do
+ *      for j <- 0 to n-1-i do
+ *          if a[j] > a[j+1]
+ *              swap a[j] with a[j+1]
+ * the main() takes size of array and the array itself as inputs from user
+ *  calls the bubbleSort() to arrange the array in ascending order using bubble sort technique
+ *  display the arranged array with appropriate output message
+ */
