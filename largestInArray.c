21ca31c1d331f384654a92db2ca6736c9936cd89
nawseekhiya <araj80317@gmail.com>
2024-10-20 14:57:45 +0530

    Add largestInArray.c algorithm

---
+++ largestInArray.c
@@ -0,0 +1,12 @@
+/**
+ * ALGORITHM findLargest(a,size)
+ *  a: array of positive integers
+ *  size: length of the array a
+ * assume a[0] be largest
+ * for i <- 1 to size-1 do
+ *      check if a[i] > largest
+ *          largest <- a[i]
+ * return largest
+ * the main() takes input of size of array, elements of array
+ * and display the output message by calling findLargest(a,size) 
+ */
