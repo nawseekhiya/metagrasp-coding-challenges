4ce38996963f5f3e6e8b1b211a54e6ef8e48cce9
nawseekhiya <araj80317@gmail.com>
2024-10-20 14:42:12 +0530

    Add anagramChecker.java algorithm

---
+++ anagramChecker.java
@@ -0,0 +1,12 @@
+/**
+ * ALGORITHM anagramChecker(s1,s2)
+ *  s1: a string taken as input from user
+ *  s2: a string taken as input from user
+ *       refines the string (like converting to lowercase, removing whitespaces)
+ *       compares length of both strings (not equal means they cannot be anagram)
+ *       sorts both the strings
+ *       compares if they are equal (equal means anagram)
+ * the main() takes two user inputs as two individual strings,
+ * compares if anagram by calling anagramChecker() parameterized with s1,s2
+ * displays the output message appropriately
+ */
