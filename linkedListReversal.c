0f4168a724c66f290cd1557be6ef9fce3c428f4b
nawseekhiya <araj80317@gmail.com>
2024-10-20 14:31:43 +0530

    Add linkedListReversal.c source code

--- linkedListReversal.c
+++ linkedListReversal.c
@@ -1,6 +1,5 @@
 /**
- * /**
- * ALGORITHM linkedListReversal(head)
+ * ALGORITHM reverse(head)
  *  head: the head node of the linked list
  *      reverses the order of nodes in the list
  *  prev <- NULL
@@ -14,4 +13,65 @@
  *  head <- prev                 // sets the new head of the reversed list
  *  return head
  */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+// structure the node is defined
+struct Node {
+    int data;
+    struct Node* next;
+};
+
+// this functions reverse the nodes of the list
+struct Node* reverse(struct Node* head) {
+    struct Node* prev = NULL;
+    struct Node* curr = head;
+    struct Node* next = NULL;
+
+    while (curr != NULL) {
+        next = curr->next;
+        curr->next = prev;
+        prev = curr;
+        curr = next;
+    }
+    return prev;
+}
+
+// this function inserts a node at the beginning of the list
+void push(struct Node** headRef, int newData) {
+    struct Node* newNode = (struct Node*) malloc(sizeof(struct Node));
+    newNode->data = newData;
+    newNode->next = (*headRef);
+    (*headRef) = newNode;
+}
+
+// this function prints the linked list
+void printList(struct Node* node) {
+    while (node != NULL) {
+        printf("%d ", node->data);
+        node = node->next;
+    }
+    printf("\n");
+}
+
+int main() {
+    struct Node* head = NULL;
+    int size,val;
+    printf("Enter size of linked list: ");
+    scanf("%d",&size);
+    printf("Enter %d values to push them to linked list: ",size);
+    for(int i=0;i<size;i++)
+    {
+        scanf("%d",&val);
+        push(&head, val);
+    }
+    printf("Original Linked List:\n");
+    printList(head);
+    head = reverse(head);
+    printf("Reversed Linked List:\n");
+    printList(head);
+    return 0;
+}
+
 
