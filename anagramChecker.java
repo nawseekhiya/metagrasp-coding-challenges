99f681c3b8d1c531ff2da7406d07351af69605b0
nawseekhiya <araj80317@gmail.com>
2024-10-20 14:50:15 +0530

    Add anagramChecker.java source code

--- anagramChecker.java
+++ anagramChecker.java
@@ -10,3 +10,36 @@
  * compares if anagram by calling anagramChecker() parameterized with s1,s2
  * displays the output message appropriately
  */
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+class anagramChecker {
+    public boolean areAnagrams(String s1, String s2) {
+        s1 = s1.replaceAll("\\s", "").toLowerCase();
+        s2 = s2.replaceAll("\\s", "").toLowerCase();
+        if (s1.length() != s2.length()) {
+            return false;
+        }
+        char a1[] = s1.toCharArray();
+        char a2[] = s2.toCharArray();
+        Arrays.sort(a1);
+        Arrays.sort(a2);
+        return Arrays.equals(a1, a2);
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter the string1: ");
+        String s1 = in.nextLine();
+        System.out.println("Enter the string2: ");
+        String s2 = in.nextLine();
+        anagramChecker obj = new anagramChecker();
+        if (obj.areAnagrams(s1, s2)) {
+            System.out.println(s1 + " and " + s2 + " are anagrams.");
+        } else {
+            System.out.println(s1 + " and " + s2 + " are not anagrams.");
+        }
+        in.close();
+    }
+}
