ebffa26680010cccea6b27a6c693c5a1804d55a8
nawseekhiya <araj80317@gmail.com>
2024-10-20 14:16:49 +0530

    Add sumOfEvenNumbers.c algorithm

---
+++ sumOfEvenNumbers.c
@@ -0,0 +1,11 @@
+/**
+ * ALGORITHM sumOfEvenNumbers(n)
+ *  n: a positive array of integers taken as input from user
+ *      finds even integers in the array
+ *      calculates the sum of those integers
+ *  let len be length of array n
+ * for i<-0 to len-1 do
+ *      if n[i] % 2 = 0
+ *          sum+=n[i]
+ * return sum
+ */
