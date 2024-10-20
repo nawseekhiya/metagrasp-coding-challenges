1533295e5384aea9b4ee7486ffd39b0d420deb9f
nawseekhiya <araj80317@gmail.com>
2024-10-20 13:26:43 +0530

    Add bubbleSort.c source code

--- bubbleSort.c
+++ bubbleSort.c
@@ -11,3 +11,40 @@
  *  calls the bubbleSort() to arrange the array in ascending order using bubble sort technique
  *  display the arranged array with appropriate output message
  */
+
+
+#include <stdio.h>
+void bubbleSort(int a[], int len)
+{
+    for (int i = 0; i < len - 1; i++)
+    {
+        for (int j = 0; j < len - 1 - i; j++)
+        {
+            if (a[j] > a[j + 1])
+            {
+                int temp = a[j]; // temp var is used for swapping purpose
+                a[j] = a[j + 1];
+                a[j + 1] = temp;
+            }
+        }
+    }
+}
+
+int main()
+{
+    int size;
+    printf("Enter the no. of elements: ");
+    scanf("%d", &size);
+    int a[size];
+    printf("Enter %d elements in the array: \n", size);
+    for(int i=0; i<size; i++) {
+        scanf("%d", &a[i]);
+    }
+    bubbleSort(a,size);
+    printf("The sorted array in ascending order using Bubble Sort:\n");
+    for (int i = 0; i < size; i++)
+    {
+        printf("%d\t",a[i]);
+    }
+    return 0;
+}
