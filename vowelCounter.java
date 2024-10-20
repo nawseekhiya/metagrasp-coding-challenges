2676c2538cf0c6a172c310dff39b94f5a4de8731
nawseekhiya <araj80317@gmail.com>
2024-10-20 13:54:40 +0530

    Add vowelCounter.java source code

--- vowelCounter.java
+++ vowelCounter.java
@@ -1,3 +1,4 @@
+
 /**
  * ALGORITHM vowelCounter(s)
  *  s: a String input from user may/may not consist vowels
@@ -13,3 +14,31 @@
  * gets the value of the counter from vowelCounter()
  * displays the no. of vowels with appropriate output message
  */
+
+import java.util.Scanner;
+class vowelCounter {
+    public int vCounter(String s) {
+        int c = 0;
+        s = s.toLowerCase();
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+            if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') {
+                c++; // incrementing vowel counter if vowel found
+            }
+        }
+        return c;
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter a string (word or sentence) : ");
+        String s = in.nextLine();
+        vowelCounter obj = new vowelCounter();
+        int vowels = obj.vCounter(s);
+        if (vowels < 0)
+            System.out.println("No vowels found.");
+        else
+            System.out.println(+vowels + " vowels found.");
+        in.close();
+    }
+}
